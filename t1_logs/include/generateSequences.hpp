8b877e75be7c5e831e1e65436023a42d465020b5
M4rb4r0 <martin.avendano.f@ug.uchile.cl>
2025-05-11 22:16:32 -0400

    Fixed a comment in generateSequences

--- t1_logs/include/generateSequences.hpp
+++ t1_logs/include/generateSequences.hpp
@@ -1,7 +1,7 @@
 #include <cstdint>
 #include <string>
 
-/// @brief Genera 5 secuencias de números al azar y las almacena en un archivos
+/// @brief Genera secuencias de números al azar y las almacena en archivos
 /// binarios.
 /// @param N Tamaño del archivo en bytes
 /// @param filename Archivo de salida.
